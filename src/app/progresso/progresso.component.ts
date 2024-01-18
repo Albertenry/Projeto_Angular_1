975bb45c5301cb2dbd1b43042c1150aa3319df2a
Albertenry <albertenry2@gmail.com>
2024-01-17 22:18:58 -0300

    commit 01

---
+++ src/app/progresso/progresso.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-progresso',
+  templateUrl: './progresso.component.html',
+  styleUrl: './progresso.component.css'
+})
+export class ProgressoComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+
+}
